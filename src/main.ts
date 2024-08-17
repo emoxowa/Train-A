2eb11d07dea4abe05df0b8c69f8e22f8f71208dc
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-08-17 12:05:14 +0300

    feat: add airbnb

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { startServer } from '@planess/train-a-backend';
+import { AppComponent } from '@app/app.component';
+import { appConfig } from '@app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+startServer()
+  .then(() => bootstrapApplication(AppComponent, appConfig))
+  .catch((err: Error) => console.error(err));
