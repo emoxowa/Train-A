40228dac539cadaff1e1ea65b3f992df25024b39
Denis Svetleishii <74614852+IamBigBobby@users.noreply.github.com>
2024-08-20 22:04:11 +0400

    Kan 2 core component setup (#2)
    * feat: install @planess/train-a-backend
    
    * feat: add base app structure
    
    * feat: add base ngrx store

---
+++ src/app/shared/components/shared-component/shared-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './shared-component.component.html',
+  styleUrl: './shared-component.component.scss'
+})
+export class SharedComponentComponent {
+
+}
---
+++ src/app/train/train-component/train-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-train-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './train-component.component.html',
+  styleUrl: './train-component.component.scss'
+})
+export class TrainComponentComponent {
+
+}
