9e8717315613cd04c3eb60f5d5ab77838c004498
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-08-22 13:13:53 +0300

    feat: add dataList

--- src/app/train/components/search-form/search-form.component.ts
+++ src/app/train/components/search-form/search-form.component.ts
@@ -2,9 +2,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TuiButton, TuiIcon, TuiIconPipe } from '@taiga-ui/core';
+import { TuiButton, TuiDataList, TuiIcon, TuiIconPipe } from '@taiga-ui/core';
 import { TuiInputModule, TuiInputDateModule, TuiInputTimeModule } from '@taiga-ui/legacy';
-import { TuiDay } from '@taiga-ui/cdk';
+import { TUI_DEFAULT_MATCHER, TuiDay, TuiLet } from '@taiga-ui/cdk';
+import { map, Observable, of, startWith, switchMap } from 'rxjs';
+
+const CITIES = [
+  { name: 'Moscow', latitude: 55.7558, longitude: 37.6176 },
+  { name: 'New York', latitude: 40.7128, longitude: -74.006 },
+  { name: 'London', latitude: 51.5074, longitude: -0.1278 },
+  { name: 'Tokyo', latitude: 35.6762, longitude: 139.6503 },
+];
+
+function request(query: string): Observable<readonly { name: string; latitude: number; longitude: number }[]> {
+  return of(CITIES.filter((item) => TUI_DEFAULT_MATCHER(item.name, query)));
+}
 
 @Component({
   selector: 'app-search-form',
@@ -18,6 +30,8 @@ import { TuiDay } from '@taiga-ui/cdk';
     TuiIcon,
     TuiIconPipe,
     TuiInputTimeModule,
+    TuiLet,
+    TuiDataList,
   ],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.scss',
@@ -49,4 +63,36 @@ export class SearchFormComponent {
     this.form.get('from')?.setValue(toValue);
     this.form.get('to')?.setValue(fromValue);
   }
+
+  protected readonly itemsFrom$ = this.form.get('from')!.valueChanges.pipe(
+    startWith(''),
+    switchMap((value) =>
+      request(value ?? '').pipe(
+        map((response) => {
+          if (response.length === 1 && String(response[0].name) === value) {
+            this.form.get('from')!.setValue(response[0].name);
+            return [];
+          }
+          return response;
+        })
+      )
+    ),
+    startWith(CITIES)
+  );
+
+  protected readonly itemsTo$ = this.form.get('to')!.valueChanges.pipe(
+    startWith(''),
+    switchMap((value) =>
+      request(value ?? '').pipe(
+        map((response) => {
+          if (response.length === 1 && String(response[0].name) === value) {
+            this.form.get('to')!.setValue(response[0].name);
+            return [];
+          }
+          return response;
+        })
+      )
+    ),
+    startWith(CITIES)
+  );
 }
