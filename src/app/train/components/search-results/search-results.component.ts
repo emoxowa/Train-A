dd507127d7144646f0bde659a3f65d6530bf5408
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-08-22 22:20:00 +0300

    feat: add SearchResultsComponent

--- src/app/train/components/search-results/search-results.component.ts
+++ src/app/train/components/search-results/search-results.component.ts
@@ -1,10 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { searchResponse } from '@app/train/models/search-response.model';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
+import { FormatDurationPipe } from '@app/train/pipes/format-duration.pipe';
+import { NoRidesAvailableComponent } from '../no-rides-available/no-rides-available.component';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TuiButton, TuiIcon, NoRidesAvailableComponent, FormatDurationPipe],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.scss',
 })
-export class SearchResultsComponent {}
+export class SearchResultsComponent {
+  searchResponse = searchResponse;
+
+  onRouteClick(route: number[]): void {
+    // eslint-disable-next-line no-console
+    console.log('Route clicked:', route, this.searchResponse);
+  }
+}
--- src/app/train/models/search-response.model.ts
+++ src/app/train/models/search-response.model.ts
@@ -6,3 +6,58 @@ export interface SearchResponse {
   to: StationData;
   routes: Route[];
 }
+
+export const searchResponse: SearchResponse = {
+  from: {
+    stationId: 1,
+    city: 'City45',
+    geolocation: { latitude: 55.7558, longitude: 37.6173 },
+  },
+  to: {
+    stationId: 2,
+    city: 'City102',
+    geolocation: { latitude: 40.7128, longitude: -74.006 },
+  },
+  routes: [
+    {
+      id: 1,
+      path: [1, 2],
+      carriages: ['1st class', '2nd class'],
+      schedule: [
+        {
+          rideId: 101,
+          segments: [
+            {
+              time: ['2024-08-13T16:30:00Z', '2024-08-13T22:26:00Z'],
+              price: { '1st class': 731, '2nd class': 498 },
+              occupiedSeats: [1, 2, 3],
+            },
+          ],
+          time: ['2024-08-13T16:30:00Z', '2024-08-13T22:26:00Z'],
+          price: { '1st class': 731, '2nd class': 498 },
+          occupiedSeats: [1, 2, 3],
+        },
+      ],
+    },
+    {
+      id: 2,
+      path: [1, 2],
+      carriages: ['Women-only'],
+      schedule: [
+        {
+          rideId: 102,
+          segments: [
+            {
+              time: ['2024-08-13T19:05:00Z', '2024-08-14T02:23:00Z'],
+              price: { 'Women-only': 586 },
+              occupiedSeats: [4],
+            },
+          ],
+          time: ['2024-08-13T19:05:00Z', '2024-08-14T02:23:00Z'],
+          price: { 'Women-only': 586 },
+          occupiedSeats: [4],
+        },
+      ],
+    },
+  ],
+};
