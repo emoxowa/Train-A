4a59249bb17423ab2368a6c526d7bf2bbb946431
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-08-26 17:26:44 +0300

    feat: add Redux DevTools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { StoreDevtoolsModule, provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
 import { appReducer } from './core/store/app.reducer';
 import { initialAppState } from './core/store/app-state';
 import { StationEffectService } from './core/store/admin-store/effects/station-effect.service';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,5 +22,12 @@ export const appConfig: ApplicationConfig = {
     NG_EVENT_PLUGINS,
     provideStore(appReducer, { initialState: initialAppState }),
     provideEffects([StationEffectService]),
+    provideStoreDevtools({ maxAge: 25, logOnly: environment.production }),
+    importProvidersFrom(
+      StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: false,
+      })
+    ),
   ],
 };
