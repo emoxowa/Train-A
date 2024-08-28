c1fee2bea38762a63f42527de0b836237c82caf6
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-28 20:41:36 +0400

    feat: add index for seat

--- src/app/admin/pages/carriages/components/carriages-dynamic-from-create/carriages-dynamic-from-create.component.ts
+++ src/app/admin/pages/carriages/components/carriages-dynamic-from-create/carriages-dynamic-from-create.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ICarriagesType } from '@app/admin/models/create-new-carriage-type.model';
 import { CarriageActions } from '@app/core/store/admin-store/actions/carriage.actions';
+import { CarriageComponent } from '@app/shared/components/carriage/carriage.component';
 import { Store } from '@ngrx/store';
 import { TuiButton } from '@taiga-ui/core';
 import { TuiInputModule } from '@taiga-ui/legacy';
@@ -9,33 +10,35 @@ import { TuiInputModule } from '@taiga-ui/legacy';
 @Component({
   selector: 'app-carriages-dynamic-from-create',
   standalone: true,
-  imports: [TuiButton, ReactiveFormsModule, TuiInputModule],
+  imports: [TuiButton, ReactiveFormsModule, TuiInputModule, CarriageComponent],
   template: `
     <div>
-      <p>Name: {{ createCarriagesForm.get('name')?.value }}</p>
+      <!-- <p>Name: {{ createCarriagesForm.get('name')?.value }}</p>
       <p>Rows: {{ createCarriagesForm.get('rows')?.value }}</p>
       <p>Left Seats: {{ createCarriagesForm.get('leftSeats')?.value }}</p>
       <p>Right Seats: {{ createCarriagesForm.get('rightSeats')?.value }}</p>
+    </div> -->
+      <app-carriage [carriagesData]="carriagesData"></app-carriage>
+      <form class="carriages__edit-form" [formGroup]="createCarriagesForm" (ngSubmit)="createStation()">
+        <tui-input placeholder="enter new name" formControlName="name">
+          name
+          <input tuiTextfieldLegacy placeholder="enter new name" type="text" />
+        </tui-input>
+        <tui-input formControlName="rows">
+          rows
+          <input tuiTextfieldLegacy placeholder="enter new rows" type="number" />
+        </tui-input>
+        <tui-input formControlName="leftSeats">
+          left seats
+          <input tuiTextfieldLegacy placeholder="enter new left seats" type="number" />
+        </tui-input>
+        <tui-input formControlName="rightSeats">
+          right seats
+          <input tuiTextfieldLegacy placeholder="enter new right seats" type="number" />
+        </tui-input>
+        <button size="s" tuiButton>Create carriage</button>
+      </form>
     </div>
-    <form class="carriages__edit-form" [formGroup]="createCarriagesForm" (ngSubmit)="createStation()">
-      <tui-input placeholder="enter new name" formControlName="name">
-        name
-        <input tuiTextfieldLegacy placeholder="enter new name" type="text" />
-      </tui-input>
-      <tui-input formControlName="rows">
-        rows
-        <input tuiTextfieldLegacy placeholder="enter new rows" type="number" />
-      </tui-input>
-      <tui-input formControlName="leftSeats">
-        left seats
-        <input tuiTextfieldLegacy placeholder="enter new left seats" type="number" />
-      </tui-input>
-      <tui-input formControlName="rightSeats">
-        right seats
-        <input tuiTextfieldLegacy placeholder="enter new right seats" type="number" />
-      </tui-input>
-      <button size="s" tuiButton>Create carriage</button>
-    </form>
   `,
   styleUrl: './carriages-dynamic-from-create.component.scss',
 })
@@ -51,16 +54,19 @@ export class CarriagesDynamicFromCreateComponent {
     rightSeats: [''],
   });
 
-  // eslint-disable-next-line class-methods-use-this
-  createStation() {
-    const createNewCarriage: ICarriagesType = {
+  get carriagesData(): ICarriagesType {
+    return {
       code: this.createCarriagesForm.get('name')?.value,
       name: this.createCarriagesForm.get('name')?.value,
       rows: this.createCarriagesForm.get('rows')?.value,
       leftSeats: this.createCarriagesForm.get('leftSeats')?.value,
       rightSeats: this.createCarriagesForm.get('rightSeats')?.value,
     };
+  }
 
+  // eslint-disable-next-line class-methods-use-this
+  createStation() {
+    const createNewCarriage: ICarriagesType = this.carriagesData;
     this.store.dispatch(CarriageActions.createNewCarriageType({ newCarriages: createNewCarriage }));
   }
 }
--- src/app/shared/components/carriage/carriage.component.ts
+++ src/app/shared/components/carriage/carriage.component.ts
@@ -15,20 +15,22 @@ import { ICarriagesType } from '@app/admin/models/create-new-carriage-type.model
         @for (row of getRows(carriagesData.rows); track row; let rowIndex = $index) {
           <div class="carriage__row">
             <div class="carriage__left-seats">
-              @for (seat of getSeats(carriagesData.leftSeats, rowIndex); track seat; let seatIndexLeft = $index) {
+              @for (seat of getSeats(carriagesData.leftSeats); track seat; let seatIndexLeft = $index) {
                 <div class="carriage__seat">
-                  L{{ row }}-{{ seatIndexLeft + rowIndex * (carriagesData.leftSeats + carriagesData.rightSeats) + 1 }}
+                  {{
+                    seat +
+                      (getSeats(carriagesData.leftSeats).length + getSeats(carriagesData.rightSeats).length) * rowIndex
+                  }}
                 </div>
               }
             </div>
             <div class="carriage__right-seats">
-              @for (seat of getSeats(carriagesData.rightSeats, rowIndex); track seat; let seatIndexRight = $index) {
+              @for (seat of getSeats(carriagesData.rightSeats); track seat; let seatIndexRight = $index) {
                 <div class="carriage__seat">
-                  R{{ row }}-{{
-                    seatIndexRight +
-                      carriagesData.leftSeats +
-                      rowIndex * (carriagesData.leftSeats + carriagesData.rightSeats) +
-                      1
+                  {{
+                    getSeats(carriagesData.leftSeats).length +
+                      seat +
+                      (getSeats(carriagesData.rightSeats).length + getSeats(carriagesData.leftSeats).length) * rowIndex
                   }}
                 </div>
               }
@@ -45,11 +47,13 @@ export class CarriageComponent {
 
   // eslint-disable-next-line class-methods-use-this
   getRows(count: number): number[] {
-    return Array.from({ length: count }, (_, index) => index + 1);
+    const rows = Array.from({ length: count }, (_, index) => index + 1);
+    return rows;
   }
 
   // eslint-disable-next-line class-methods-use-this
-  getSeats(count: number, rowIndex: number): number[] {
-    return Array.from({ length: count }, (_, index) => index + rowIndex * count);
+  getSeats(count: number): number[] {
+    const seats = Array.from({ length: count }, (_, index) => index + 1);
+    return seats;
   }
 }
