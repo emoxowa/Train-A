d566d41973ad485d40f124e2e12bcb9b5a1c5310
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-29 14:45:31 +0400

    feat: add service reqests for routes

--- src/app/admin/service/admin.service.ts
+++ src/app/admin/service/admin.service.ts
@@ -5,6 +5,7 @@ import { ICreateStationResponse, IStation } from '../models/station-list.model';
 import { ICreateStation } from '../models/create-station.model';
 import { IAdminToken, ICreateAdmin } from '../models/create-admin';
 import { ICarriagesType } from '../models/create-new-carriage-type.model';
+import { IRoutes } from '../models/routes.model';
 
 @Injectable({
   providedIn: 'root',
@@ -55,6 +56,30 @@ export class AdminService {
         Authorization: `Bearer ${this.token$.value}`,
       },
     });
+  }
+
+  getRoutes(): Observable<IRoutes> {
+    return this.http.get<IRoutes>('/api/route');
+  }
+
+  createRoute(newRoute: IRoutes): Observable<IRoutes> {
+    return this.http.post<IRoutes>('api/route', newRoute, {
+      headers: {
+        Authorization: `Bearer ${this.token$.value}`,
+      },
+    });
+  }
+
+  updRoutes(IdRoute: number, updRoute: IRoutes): Observable<IRoutes> {
+    return this.http.put<IRoutes>(`/api/route/${IdRoute}`, updRoute, {
+      headers: {
+        Authorization: `Bearer ${this.token$.value}`,
+      },
+    });
+  }
+
+  deleteRoute(IdRoute: number): Observable<void> {
+    return this.http.delete<void>(`/api/route/${IdRoute}`);
   }
 
   // it's for developing
