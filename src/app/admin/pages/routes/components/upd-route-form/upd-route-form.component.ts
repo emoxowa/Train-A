a0aaf2f7016106171a3c2f3ca36bc20434eefa9e
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-31 19:52:18 +0400

    feat: add upd form route

---
+++ src/app/admin/pages/routes/components/upd-route-form/upd-route-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upd-route-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      upd-route-form works!
+    </p>
+  `,
+  styleUrl: './upd-route-form.component.scss'
+})
+export class UpdRouteFormComponent {
+
+}
