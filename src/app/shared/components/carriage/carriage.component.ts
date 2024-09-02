ffceb67ee199b32ebfe1aff6e2e36e576b0f8d93
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-09-02 16:17:47 +0300

    feat: change carriage styles

--- src/app/shared/components/carriage/carriage.component.ts
+++ src/app/shared/components/carriage/carriage.component.ts
@@ -1,35 +1,48 @@
 import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { ICarriage } from '@app/admin/models/create-new-carriage-type.model';
+import { TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-carriage',
   standalone: true,
-  imports: [],
+  imports: [TuiIcon],
   template: `
     <div class="carriage">
       <div class="carriage__header">
-        <span>Name: {{ carriagesData.name }}</span>
-        <span>Sits: {{ (leftSeats.length + rightSeats.length) * rows.length }}</span>
+        <span>{{ carriagesData.name }}</span>
+        <div class="carriage__seats">{{ (leftSeats.length + rightSeats.length) * rows.length }} seats</div>
       </div>
-      <div class="carriage__body">
-        @for (row of rows; track row; let rowIndex = $index) {
-          <div class="carriage__row">
-            <div class="carriage__left-seats">
-              @for (seat of leftSeats; track seat; let seatIndexLeft = $index) {
-                <div class="carriage__seat">L-{{ seat + (leftSeats.length + rightSeats.length) * rowIndex }}</div>
-              }
+      <div class="carriage__container">
+        <div class="carriage__enter">
+          <tui-icon icon="@tui.fa.solid.mug-saucer"></tui-icon>
+          <tui-icon icon="@tui.fa.solid.person-walking-dashed-line-arrow-right"></tui-icon>
+          <tui-icon icon="@tui.fa.solid.trash" style="align-self: flex-end;"></tui-icon>
+          <tui-icon icon="@tui.fa.solid.restroom" style="align-self: flex-end;"></tui-icon>
+        </div>
+        <div class="carriage__body">
+          @for (row of rows; track row; let rowIndex = $index) {
+            <div class="carriage__row">
+              <div class="carriage__left-seats">
+                @for (seat of leftSeats; track seat; let seatIndexLeft = $index) {
+                  <div class="carriage__seat">L-{{ seat + (leftSeats.length + rightSeats.length) * rowIndex }}</div>
+                }
+              </div>
+
+              <div class="carriage__right-seats">
+                @for (seat of rightSeats; track seat; let seatIndexRight = $index) {
+                  <div class="carriage__seat">
+                    R-{{
+                      getSeats(carriagesData.leftSeats).length +
+                        seat +
+                        (leftSeats.length + rightSeats.length) * rowIndex
+                    }}
+                  </div>
+                }
+              </div>
             </div>
-            <div class="carriage__right-seats">
-              @for (seat of rightSeats; track seat; let seatIndexRight = $index) {
-                <div class="carriage__seat">
-                  R-{{
-                    getSeats(carriagesData.leftSeats).length + seat + (leftSeats.length + rightSeats.length) * rowIndex
-                  }}
-                </div>
-              }
-            </div>
-          </div>
-        }
+          }
+        </div>
+        <div class="carriage__restroom"><tui-icon icon="@tui.fa.solid.restroom"></tui-icon></div>
       </div>
     </div>
   `,
