49b119692d745246985288126444b88a8585785c
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-09-03 08:51:31 +0300

    feat: add price

--- src/app/train/pipes/sumCarriagePrice.pipe.ts
+++ src/app/train/pipes/sumCarriagePrice.pipe.ts
@@ -7,7 +7,10 @@ import { IScheduleRideInformation } from '../models/ride-information.model';
   standalone: true,
 })
 export class SumCarriagePricePipe implements PipeTransform {
-  transform(schedule: ISchedule | IScheduleRideInformation, carriageType: string): number {
+  transform(schedule: ISchedule | IScheduleRideInformation, carriageType: string | null): number {
+    if (carriageType === null) {
+      return 0;
+    }
     return schedule.segments.reduce((total, segment) => {
       return total + (segment.price[carriageType] || 0);
     }, 0);
